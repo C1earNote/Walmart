09a47a8dd501fe9921482ebeb748dbc2267038b6
FaYMan2 <suavarnavats244@gmail.com>
2025-07-13 20:16:51 +0530

    some fixes

--- Supplier_risk_predictor/config.py
+++ Supplier_risk_predictor/config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 GNEWS_API_KEY = os.getenv("GNEWS_API_KEY")
 GNEWS_ENDPOINT = "https://gnews.io/api/v4/search"
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 RISK_KEYWORDS = [
     "strike", "protest", "labour unrest", "shutdown", "fire", "flood",
